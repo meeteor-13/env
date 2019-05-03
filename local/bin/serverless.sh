0e2de22c3b1ec3021b4ac1cd33879326fce6382e
Alexander Babai <aliaksandr.babai@gmail.com>
2019-05-03 11:58:41 +0300

    refactor(local): remove redundant var

--- local/bin/serverless.sh
+++ local/bin/serverless.sh
@@ -1,5 +1,5 @@
 source ./bin/helpers.sh
 
-DOCKER_COMPOSE_FILE="docker-compose.serverless.yml"
+DOCKER_COMPOSE_OVERRIDE_PATH="-f docker-compose.serverless.yml $DOCKER_COMPOSE_OVERRIDE_PATH"
 
 start
